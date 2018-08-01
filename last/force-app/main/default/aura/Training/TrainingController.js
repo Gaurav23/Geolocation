46a1297d6b22c90a00e967b954f9eae7d10ee3ed
Gaurav <ptwo.patel@gmail.com>
2018-08-01 12:07:39 +0530

    Map Component Added

--- last/force-app/main/default/aura/Training/TrainingController.js
+++ last/force-app/main/default/aura/Training/TrainingController.js
@@ -1,5 +1,12 @@
 ({
-    myAction : function(component, event, helper) {
+	doInit: function(component, event, helper) {      
 
-    }
+       var action = component.get('c.getMyMap');
+        
+        action.setCallback(this,function(response){
+            alert(JSON.stringify(response.getReturnValue()));
+            component.set('v.myMap',response.getReturnValue());
+        });
+        $A.enqueueAction(action);
+     }
 })
